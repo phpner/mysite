798cccc8c7f13a121a9ec751b224c76db9e2d32e
phpner <phpner@gmil.com>
2018-06-28 12:52:41 +0300

    work on my works

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(autoprefixer({
+             browsers: ['last 99 versions'],
+             cascade: false
+         }))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', function(){
+
+    browserSync.init({
+         proxy: "http://mypor.ru"
+    });
+
+    gulp.watch('./sass/*.scss', ['sass']);
+    
+    gulp.watch(["js/*.js",'css/*.css','*.html','*/*.js']).on('change', browserSync.reload);
+});
